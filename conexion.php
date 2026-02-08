e2c8f62aa64a929361769639d321bb8401a12710
AdrianRiveroll <adrianriceeoll981@gmail.com>
2026-02-08 00:26:32 -0600

    Quite mi contrasena en conexionphp

--- conexion.php
+++ conexion.php
@@ -7,6 +7,6 @@ $base_datos = "escuela";
 $conexion = new mysqli($host, $usuario, $password, $base_datos);
 
 if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+    die("Error de conexion: " . $conexion->connect_error);
 }
 ?>
